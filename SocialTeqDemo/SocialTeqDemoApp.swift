31d60f426d26697f8de8caaa12bca21038af81e5
Farshad Mousalou <farshadm90@gmail.com>
2021-01-15 23:12:37 +0330

    Initial Commit

---
+++ SocialTeqDemo/SocialTeqDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialTeqDemoApp.swift
+//  SocialTeqDemo
+//
+//  Created by Farshad Mousalou on 1/15/21.
+//
+
+import SwiftUI
+
+@main
+struct SocialTeqDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
